54c223cfc8e1f36171a1215f14cadba1d4e23b9f
SutterButter <aeontruths@gmail.com>
2018-01-23 15:49:04 -0500

    Added equipment group cell structure

---
+++ invention-studio-ios/EquipmentGroupCell.swift
@@ -0,0 +1,26 @@
+//
+//  EquipmentGroupCell.swift
+//  invention-studio-ios
+//
+//  Created by Noah Sutter on 1/23/18.
+//  Copyright © 2018 Invention Studio at Georgia Tech. All rights reserved.
+//
+
+import UIKit
+
+class EquipmentGroupCell: UITableViewCell {
+
+    @IBOutlet weak var equipmentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
