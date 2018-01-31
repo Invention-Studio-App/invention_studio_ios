a17e8f1fa4988738667387816bb9281e27121cb7
Nick Rupert <nickrupert7@gmail.com>
2018-01-30 22:25:52 -0500

    Added prototype cell classes

---
+++ invention-studio-ios/Views/FeedbackNameCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedbackNameCell.swift
+//  invention-studio-ios
+//
+//  Created by Nick's Creative Studio on 1/30/18.
+//  Copyright © 2018 Invention Studio at Georgia Tech. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackNameCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ invention-studio-ios/Views/FeedbackPickerHeaderCell.swift
@@ -0,0 +1,24 @@
+//
+//  FeedbackPickerHeaderCell.swift
+//  invention-studio-ios
+//
+//  Created by Nick's Creative Studio on 1/30/18.
+//  Copyright © 2018 Invention Studio at Georgia Tech. All rights reserved.
+//
+
+import UIKit
+
+class FeedbackPickerHeaderCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
