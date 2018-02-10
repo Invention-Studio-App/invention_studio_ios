a8d61efa6120fdebab4b986bb7ca8016ee049c60
Nick Rupert <nickrupert7@gmail.com>
2018-02-09 21:13:08 -0500

    Initial Commit

---
+++ invention-studio-ios/API/API.swift
@@ -0,0 +1,37 @@
+//
+//  API.swift
+//  invention-studio-ios
+//
+//  Created by Nick's Creative Studio on 2/9/18.
+//  Copyright © 2018 Invention Studio at Georgia Tech. All rights reserved.
+//
+//  Base class includes subclasses which represent the API modules
+//
+
+import Foundation
+
+class API {
+
+    let baseURL = "https://sums.gatech.edu/SUMSAPI/rest/API/"
+
+    class User {
+
+        static func Info() {
+
+        }
+
+    }
+
+    class EquipmentGroup {
+
+        static func Tools() {
+
+        }
+
+        static func Queues() {
+
+        }
+
+    }
+
+}
