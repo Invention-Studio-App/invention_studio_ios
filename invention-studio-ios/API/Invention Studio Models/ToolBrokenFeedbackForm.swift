95682905d8d6b64da1413ae3fca56f815af2144c
Nick Rupert <nickrupert7@gmail.com>
2018-04-02 20:05:46 -0400

    Initial Commit

---
+++ invention-studio-ios/API/Invention Studio Models/ToolBrokenFeedbackForm.swift
@@ -0,0 +1,20 @@
+//
+//  ToolBrokenFeedbackForm.swift
+//  invention-studio-ios
+//
+//  Created by Nick's Creative Studio on 4/2/18.
+//  Copyright © 2018 Invention Studio at Georgia Tech. All rights reserved.
+//
+
+import Foundation
+
+class ToolBrokenFeedbackForm: Codable {
+
+    var equipment_group_id: Int = 0
+    var username: String = ""
+    var problem: String = ""
+    var tool_group_name: String = ""
+    var tool_name: String = ""
+    var comments: String = ""
+
+}
